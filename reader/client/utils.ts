2b768e5eae89be376994e2511736da2b0470bf13
Henry Bravo <hbravos.info@gmail.com>
2021-11-05 11:45:24 -0500

    insecure comunication reader to writer

--- reader/client/utils.ts
+++ reader/client/utils.ts
@@ -1,47 +1,53 @@
-import { UserServiceClient } from "../proto/proto/user_grpc_pb";
-import { GeoServiceClient } from "../proto/proto/geo_grpc_pb"
-import { GridServiceClient } from "../proto/proto/grid_grpc_pb"
-import { credentials, Metadata } from "grpc";
-import { readFileSync }  from 'fs'
+import { UserServiceClient } from '../proto/proto/user_grpc_pb'
+import { GeoServiceClient } from '../proto/proto/geo_grpc_pb'
+import { GridServiceClient } from '../proto/proto/grid_grpc_pb'
+import { credentials, Metadata } from 'grpc'
+import { readFileSync } from 'fs'
 
 // const port = 3000;
-var cacert = readFileSync("./cert/server.crt");
-var cert_chain = readFileSync("./cert/server.crt");
-var private_key = readFileSync("./cert/server.key");
+var cacert = readFileSync('./cert/server.crt')
+var cert_chain = readFileSync('./cert/server.crt')
+var private_key = readFileSync('./cert/server.key')
 
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MzQzOTA1MjQsInVzZXJuYW1lIjoiZGlzcGF0Y2hlciIsInJvbGUiOiJESVMifQ.b_qDF3c_MD9AJNAM8GcVB57hrQD3TvMtzRbP9DqYgbg'
+const token =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MzQzOTA1MjQsInVzZXJuYW1lIjoiZGlzcGF0Y2hlciIsInJvbGUiOiJESVMifQ.b_qDF3c_MD9AJNAM8GcVB57hrQD3TvMtzRbP9DqYgbg'
 
 function newChannelCredentials() {
-  return credentials.combineChannelCredentials(
+  return credentials.createInsecure()
+
+  /* credentials.combineChannelCredentials(
+    credentials.createInsecure(),
     credentials.createSsl(
       cacert,
       private_key,
       cert_chain
     ),
     credentials.createFromMetadataGenerator((params, callback) => {
-      const metadata = new Metadata();
-      metadata.add('authorization', token);
-      callback(null, metadata);
-    })
-  );
+      const metadata = new Metadata()
+      metadata.add('authorization', token)
+      callback(null, metadata)
+    }),
+  )*/
 }
 
 const cred = newChannelCredentials()
 
 export const client = new GeoServiceClient(
-  `dts.pe:9003`, cred, {
-    'grpc.ssl_target_name_override': 'dts.pe',
-    "grpc.default_authority": 'dts.pe'
-  }
-);
-console.log("client", client)
+  `dts.pe:9003`,
+  cred /* {
+  'grpc.ssl_target_name_override': 'dts.pe',
+  'grpc.default_authority': 'dts.pe',
+}*/,
+)
+console.log('client', client)
 
 export const clientGrid = new GridServiceClient(
-  `dts.pe:9003`, cred, {
-    'grpc.ssl_target_name_override': 'dts.pe',
-    "grpc.default_authority": 'dts.pe'
-  }
-);
-console.log("clientGrid", clientGrid)
+  `dts.pe:9003`,
+  cred /*{
+  'grpc.ssl_target_name_override': 'dts.pe',
+  'grpc.default_authority': 'dts.pe',
+}*/,
+)
+console.log('clientGrid', clientGrid)
 
-export const noop = () => {};
+export const noop = () => {}
