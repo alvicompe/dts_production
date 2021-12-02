6daad66519af55e2e21efd93896d999c45a2daf4
Henry Bravo <hbravos.info@gmail.com>
2021-12-02 18:25:26 -0500

    reader stock

--- reader/business/GeoReader.ts
+++ reader/business/GeoReader.ts
@@ -1,24 +1,22 @@
-import {TreeReader} from "../client/treeReader";
-import {BuildTree} from "./BuildTree";
-import UploadGeo from "../client/geo";
-import {ValidationTree} from "../client/validationTree";
+import { TreeReader } from '../client/treeReader'
+import UploadGeo from '../client/geo'
+import { ValidationTree } from '../client/validationTree'
 
 export class GeoReader {
+  readFiles() {
+    const tree = new ValidationTree()
+    return tree.getValidatedTree()
+  }
 
-    readFiles(){
-        const tree = new ValidationTree();
-        return tree.getValidatedTree();
+  async createGeo() {
+    const tree = new TreeReader()
+    let { geo, status, message, errors } = await tree.buildGeo()
+    if (status) {
+      const okUploadGeo = await UploadGeo(JSON.stringify(geo))
+      message = okUploadGeo
+        ? 'Successful creation'
+        : 'Something went wrong in the creation'
     }
-
-    async createGeo() {
-        const tree = new TreeReader()
-        let {geo, status, message, errors} = await tree.buildGeo();
-        if (status) {
-            const okUploadGeo = await UploadGeo(JSON.stringify(geo));
-            message = (okUploadGeo) ?
-                "Successful creation" :
-                "Something went wrong in the creation";
-        }
-        return {status, message, errors}
-    }
+    return { status, message, errors }
+  }
 }
--- reader/service/GeoReader.ts
+++ reader/service/GeoReader.ts
@@ -1,41 +1,46 @@
-import {sendUnaryData, ServerUnaryCall} from "grpc";
-import {CreateTreeGeoResponse, GeoTreeResponse} from "../proto/proto/geo-reader_pb";
-import {IGeoReaderServiceServer} from "../proto/proto/geo-reader_grpc_pb";
-import {GeoReader} from "../business/GeoReader";
-import UploadGeo from "../client/geo";
-import * as google_protobuf_empty_pb from "google-protobuf/google/protobuf/empty_pb";
-import {MessageError} from "../../dts/pb_js/proto/geo-reader_pb";
-
+import { sendUnaryData, ServerUnaryCall } from 'grpc'
+import {
+  CreateTreeGeoResponse,
+  GeoTreeResponse,
+} from '../proto/proto/geo-reader_pb'
+import { IGeoReaderServiceServer } from '../proto/proto/geo-reader_grpc_pb'
+import { GeoReader } from '../business/GeoReader'
+import UploadGeo from '../client/geo'
+import * as google_protobuf_empty_pb from 'google-protobuf/google/protobuf/empty_pb'
 
 export class GeoReaderServer implements IGeoReaderServiceServer {
-    retrieveTreeGeo(call: ServerUnaryCall<google_protobuf_empty_pb.Empty>, callback: sendUnaryData<GeoTreeResponse>) {
-        const response = new GeoTreeResponse();
-        const ok = new GeoReader();
-        const result = ok.readFiles();
-        response.setGeo(result.tree);
-        if (result.response) {
-            response.setStatus(true);
-            response.setMessage("Success");
-        } else {
-            response.setStatus(false);
-            response.setMessage("Incorrect tree, check files.");
-            response.setErrorsList(result.errors);
-        }
-
-        callback(null, response);
+  retrieveTreeGeo(
+    call: ServerUnaryCall<google_protobuf_empty_pb.Empty>,
+    callback: sendUnaryData<GeoTreeResponse>,
+  ) {
+    const response = new GeoTreeResponse()
+    const ok = new GeoReader()
+    const result = ok.readFiles()
+    response.setGeo(result.tree)
+    if (result.response) {
+      response.setStatus(true)
+      response.setMessage('Success')
+    } else {
+      response.setStatus(false)
+      response.setMessage('Incorrect tree, check files.')
+      response.setErrorsList(result.errors)
     }
 
-    async createTreeGeo(call: ServerUnaryCall<google_protobuf_empty_pb.Empty>, callback: sendUnaryData<CreateTreeGeoResponse>) {
-        const geoReaderBusiness = new GeoReader();
-        const {status, message, errors} = await geoReaderBusiness.createGeo();
+    callback(null, response)
+  }
 
-        const response = new CreateTreeGeoResponse();
-        response.setStatus(status);
-        response.setMessage(message);
-        response.setErrorsList(errors);
+  async createTreeGeo(
+    call: ServerUnaryCall<google_protobuf_empty_pb.Empty>,
+    callback: sendUnaryData<CreateTreeGeoResponse>,
+  ) {
+    const geoReaderBusiness = new GeoReader()
+    const { status, message, errors } = await geoReaderBusiness.createGeo()
 
-        callback(null, response);
-    }
+    const response = new CreateTreeGeoResponse()
+    response.setStatus(status)
+    response.setMessage(message)
+    response.setErrorsList(errors)
+
+    callback(null, response)
+  }
 }
-
-
