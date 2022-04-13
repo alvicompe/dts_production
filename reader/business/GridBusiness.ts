c8179ad6186f8f8d1e29f45c7e63e53b9da61fb7
wleonch <wleonch@hotmail.com>
2022-04-13 15:57:51 -0500

    improve generate grids road

--- reader/business/GridBusiness.ts
+++ reader/business/GridBusiness.ts
@@ -25,7 +25,6 @@ export class GridBusiness {
     const response = new UpdateGridResponse()
     const { status, firstLoad, pathChange } =
       await geoReader.checkChangeGeoGrid()
-
     if (!status) {
       response.setStatus(false)
       response.setMessage("Error when reading files")
